26c8a02a13d45253eb61c842c22c12ac1da6ea64
August Saint Freytag <night@apricummedia.com>
2022-04-19 12:46:30 +0200

    Establish determining paths and dry run for renaming operations

--- Notion Postprocessor/Command.swift
+++ Notion Postprocessor/Command.swift
@@ -15,25 +15,60 @@ import ArgumentParser
 ///
 
 @main
-struct NotionPostprocessor: ParsableCommand, DirectoryEnumerator, DocumentNameRestoration {
+struct NotionPostprocessor: ParsableCommand, DirectoryEnumerator, DocumentNameProvider {
 	
 	@Argument(help: "Path to the directory exported from Notion to be processed.", completion: CompletionKind.directory)
 	var inputPath: String
 	
+	@Flag(help: "Prints out the changes that would be made to the input directory and its files but does not execute them.")
+	var dryRun: Bool = false
+	
+	// MARK: Run
+	
 	func run() throws {
 		let inputURL = URL(fileURLWithPath: inputPath, isDirectory: true).standardizedFileURL
-		let fileURLs = try markdownFileURLs(in: inputURL)
-		
-		print("File URLs:")
-		fileURLs.forEach { url in print(url.path) }
+		try processAllDocuments(in: inputURL)
 	}
+	
+	// MARK: Processing (TBD)
 	
 	func processAllDocuments(in directory: URL) throws {
-		// …
+		let fileURLs = try documentFileURLs(in: directory)
+		
+		for fileURL in fileURLs {
+			try processDocumentAndAssociates(at: fileURL)
+		}
 	}
 	
-	func processDocumentAndAssociates(_ url: URL) throws {
-		// …
+	/// Takes the document at the given URL and determines its restorable name.
+	/// Further tries to locate an associate ("nested page") directory to be processed.
+	func processDocumentAndAssociates(at url: URL) throws {
+		assert(FileManager.default.fileExists(atPath: url.path), "File manager integrity assertion failed, can not determine own path.")
+		
+		// Preparation
+		guard let canonicalDocumentName = try canonicalDocumentName(forDocumentAt: url) else {
+			throw ProcessingError(kind: .missingData, description: "Could not read canonical document name for file at path '\(url.path)'.")
+		}
+		
+		let fileName = url.lastPathComponent
+		let associateDirectoryName = try fileName.removingMatches(matching: #"\.\w+$"#)
+		
+		assert(fileName != associateDirectoryName, "Derived associate directory name transformation not valid; transform produced '\(associateDirectoryName)'.")
+		
+		let parentDirectoryURL = url.deletingLastPathComponent()
+		let associateDirectoryURL = parentDirectoryURL.appendingPathComponent(associateDirectoryName, isDirectory: true)
+		
+		let hasAssociateDirectory = FileManager.default.fileExists(atPath: associateDirectoryURL.path)
+		
+		// Execution
+		if dryRun {
+			print("Operations in path '\(parentDirectoryURL.path)':")
+			print("Document rename: '\(fileName)' → '\(canonicalDocumentName).md'")
+			
+			if hasAssociateDirectory {
+				print("Directory rename: '\(associateDirectoryName)' → '\(canonicalDocumentName)'")
+			}
+		}
 	}
 	
 }
@@ -44,26 +79,26 @@ protocol DirectoryEnumerator {}
 
 extension DirectoryEnumerator {
 	
-	func markdownFileURLs(in directory: URL) throws -> [URL] {
-		print("Getting list of files in input directory '\(directory.path)'.")
-		
+	func documentFileURLs(in directory: URL) throws -> [URL] {
 		let fileURLs = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: [], options: [.skipsHiddenFiles])
 		
-		return fileURLs
+		return fileURLs.filter { url in
+			url.lastPathComponent.contains(".md")
+		}
 	}
 	
 }
 
 // MARK: Document Names
 
-protocol DocumentNameRestoration {}
+protocol DocumentNameProvider {}
 
-extension DocumentNameRestoration {
+extension DocumentNameProvider {
 	
-	private var firstHeadingMatchPattern: String { #"# (\w+)\n"# }
+	private var firstHeadingMatchPattern: String { #"# (.+)\n"# }
 	
-	/// Reads the document at the given URL and returns its restore name if possible.
-	func restoredDocumentName(forDocumentAt fileURL: URL) throws -> String? {
+	/// Reads the document at the given URL and returns its canonical name if possible.
+	func canonicalDocumentName(forDocumentAt fileURL: URL) throws -> String? {
 		let fileData = try Data(contentsOf: fileURL)
 		
 		guard let fileContents = String(data: fileData, encoding: .utf8) else {
