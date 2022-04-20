7df103505586b04560c35781bb6b8e97acbc0836
August Saint Freytag <night@apricummedia.com>
2022-04-20 12:14:12 +0200

    Add new directory/document distinguishing enumerator function for files
    Refactor existing directory enumerator protocol.

--- Notion Postprocessor/Files/Library/DirectoryEnumerator.swift
+++ Notion Postprocessor/Files/Library/DirectoryEnumerator.swift
@@ -8,12 +8,38 @@ protocol DirectoryEnumerator {}
 
 extension DirectoryEnumerator {
 	
-	func documentFileURLs(in directory: URL) throws -> [URL] {
+	func fileURLs(in directory: URL) throws -> (documents: [URL], directories: [URL]) {
 		let fileURLs = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: [], options: [.skipsHiddenFiles])
 		
-		return fileURLs.filter { url in
-			url.lastPathComponent.contains(".md")
+		var documentURLs: [URL] = []
+		var directoryURLs: [URL] = []
+		
+		for url in fileURLs {
+			if urlIsDirectory(url) {
+				directoryURLs.append(url)
+				continue
+			}
+			
+			if urlIsDocument(url) {
+				documentURLs.append(url)
+				continue
+			}
 		}
+		
+		return (documentURLs, directoryURLs)
+	}
+	
+	func documentFileURLs(in directory: URL) throws -> [URL] {
+		let fileURLs = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: [], options: [.skipsHiddenFiles])
+		return fileURLs.filter { url in urlIsDocument(url) }
+	}
+	
+	private func urlIsDirectory(_ url: URL) -> Bool {
+		return url.hasDirectoryPath
+	}
+	
+	private func urlIsDocument(_ url: URL) -> Bool {
+		return url.lastPathComponent.contains(".md")
 	}
 	
 }
